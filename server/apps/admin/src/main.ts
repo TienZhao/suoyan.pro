ae89d1d7d86d4dc45181c4de65f233e8d176559b
Tien <zhao_t_y@sina.com>
2021-02-02 11:45:13 +0800

    Deploy swagger
    Deploy swagger at localhost:3000/api-docs

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
+  // 允许跨域
   app.enableCors()
 
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
-  console.log('http://localhost:3000')
+  console.log('http://localhost:3000/api-docs')
 }
 bootstrap();
