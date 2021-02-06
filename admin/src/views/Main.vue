4322fba0f5bd3b06109af4294f16ca9c5b8ae23c
Tien <zhao_t_y@sina.com>
2021-02-06 22:09:44 +0800

    Refine align result visualization
    Based on Echarts heatmap.

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -6,7 +6,6 @@
                 <!-- Title -->
                 <el-col :span="16" :offset="0">
                     <el-image style="width: 114px; height: 50px" :src="require('../assets/yanyes_logo_100p.png')" fit="contain" ></el-image>
-                    <!-- 所言.pro -->
                 </el-col>
                 <el-col :span="8" :offset="0" >
                 <!-- Header Navbar -->
@@ -29,11 +28,28 @@
             <!-- Main content -->
             <el-row :gutter="20">
                 <el-col :span="12" :offset="0">
+                    <el-radio-group v-model="langRadioLeft" size="medium">
+                        <el-radio-button label="汉语" ></el-radio-button>
+                    </el-radio-group>
+                </el-col>
+                <el-col :span="12" :offset="0">
+                    <el-radio-group v-model="langRadioRight" size="medium" @change="changeLangRight">
+                        <el-radio-button label="自动检测" ></el-radio-button>
+                        <el-radio-button label="英语" ></el-radio-button>
+                        <el-radio-button label="西班牙语" ></el-radio-button>
+                        <el-radio-button label="德语" ></el-radio-button>
+                        <el-radio-button label="俄语" ></el-radio-button>
+                    </el-radio-group>
+                </el-col>
+            </el-row>
+            
+            <el-row :gutter="20"  style="margin-top: 20px;">
+                <el-col :span="12" :offset="0">
                     <!-- Input Box Left -->
                     <el-input
                         type="textarea"
                         :autosize="{ minRows: 10, maxRows: 50}"
-                        placeholder="请输入中文语料"
+                        :placeholder="placeHolderLeft"
                         v-model="textareaLeft"
                         maxlength="2000"
                         show-word-limit
@@ -46,7 +62,7 @@
                     <el-input
                         type="textarea"
                         :autosize="{ minRows: 10, maxRows: 50}"
-                        placeholder="请输入西班牙语语料"
+                        :placeholder="placeHolderRight"
                         v-model="textareaRight"
                         maxlength="2000"
                         show-word-limit
@@ -63,8 +79,11 @@
                 </el-col>
                 <el-col :span="12" :offset="0"></el-col>
             </el-row>  
-
-            <div id="echart_main" style="width: 800px; height: 500px;"></div>
+            
+            <el-row :gutter="20">
+                <div id="echart_main" style="width: 1600px; height: 900px;"></div>
+            </el-row>
+            
            
         </el-main>
 
@@ -107,9 +126,26 @@
         textareaLeft = '';
         textareaRight = '';
         alignResponse;
+        echartStyle = {
+            width: '1000px',
+            height: '1000px',
+            background: '#00F',
+        };
+        langRadioLeft = '汉语';
+        langRadioRight = '英语';
+        placeHolderLeft = '请输入' + this.langRadioLeft + '语料'
+        placeHolderRight = '请输入' + this.langRadioRight + '语料'
+        changeLangRight(val) {
+            this.placeHolderRight = '请输入' + val + '语料';
+            console.log(this.placeHolderRight);
+        }
 
 
         // Methods
+        onClickCell(params){
+            console.log(params);
+        }
+
         async onAlign(){
             // alert(this.textareaLeft);
             console.log('align btn clicked');
@@ -132,7 +168,10 @@
                 })
             console.log(req)
             const res = await this.$http.post('/align',req)
-            console.log(res)
+            this.alignResponse = res;
+            console.log(res);
+
+            this.renderEchart();
         }
 
 
@@ -185,52 +224,84 @@
             var option;
             var xLegend = this.alignResponse.data.articles[0].sentenceArray;
             var yLegend = this.alignResponse.data.articles[1].sentenceArray;
-            var data = [];
-
-            console.log(xLegend);
+            var dataRaw = [];
+            var dataSeriesArray = []; // Composed of a few objects like dataSeriesTemplate{}
+            // Define Echarts data series.
+            // Document at: https://echarts.apache.org/zh/option.html#series-heatmap
+            // Each row of chart need to be placed in a different series, 
+            // Otherwise there will be an error related with 'select' event and 'selected' feature.
+            // Check details at: https://github.com/apache/echarts/issues/14230
+            var dataSeriesTemplate = {
+                name: '',
+                type: 'heatmap',
+                data: [],
+                label: {show: true},
+                selectedMode: 'multiple',
+                // Style when cursor passes by.
+                emphasis: {itemStyle: {shadowBlur: 10, shadowColor: 'rgba(0, 0, 0, 0.5)',}},
+                // Style when 'select' event occures by mouse click or 'data.selected' is set as true.
+                select: {itemStyle: {borderColor:'#ffffff', borderWidth: 4,},}
+            }
 
             this.alignResponse.data.relations.forEach(relation => {
-                var x, y, z;
+                var x, y, z
+                var dataNode = {
+                    value: [],
+                    selected: false,
+                }
                 z = parseInt(relation.similarity * 100000)/1000;
                 relation.nodes.forEach(node => {
                     if(node.articleIndex == 0){
-                        x = node.sentenceIndex
+                        x = node.sentenceIndex;
                     } else if(node.articleIndex == 1){
-                        y = node.sentenceIndex
+                        y = node.sentenceIndex;
                     }
                 })
-                const dataNode = [x, y, z]
-                data.push(dataNode)
+                dataNode.value = [y, x, z] // Replace the data.map in Echarts tutorial.
+                if (z > 70){ // Binarize align result (sample condition)
+                    dataNode.selected = true;
+                }
+                dataRaw.push(dataNode)
             });
 
-            data = data.map(function (item) {
-                return [item[1], item[0], item[2] || '-'];
-            });
+            for(var i = 0; i < yLegend.length; i++){
+                var tempDataSeries = JSON.parse(JSON.stringify(dataSeriesTemplate))
+                dataRaw.forEach(dataNode =>{
+                    if(i == dataNode.value[1]){
+                        tempDataSeries.data.push(dataNode);
+                    }
+                })
+                dataSeriesArray.push(tempDataSeries);
+            }
 
             option = {
                 tooltip: {
                     position: 'top',
                     formatter: function (params) {
-                        var xText = xLegend[params.data[0]]
-                        var yText = yLegend[params.data[1]]
-                        var zText = params.data[2]
-                        return xText + '</br>' + yText + '</br>对齐评分:' + zText
-                    }
+                        var xText = xLegend[params.data.value[0]]
+                        var yText = yLegend[params.data.value[1]]
+                        var zText = params.data.value[2]
+                        return "<div style='width: 290px; display: block; word-break: break; white-space: pre-wrap'>"+
+                            "汉语： " + xText + "</br>" +
+                            "外语： " + yText + "</br>" +
+                            "<b>对齐评分： " + zText + "</b>" + 
+                            "</div>"
+                    },
                 },
                 grid: {
                     height: '80%',
                     width: '80%',
                     top: '50px',
-                    left: '80px',
+                    left: '150px',
                 },
                 xAxis: {
                     type: 'category',
                     data: xLegend,
                     position: 'top',
-                    axisLabel:{
-                        interval:0,
-                        width: 60,
-                        overflow: 'truncate',
+                    axisLabel:{ // Text on X axis.
+                        interval:0, // Force to split neighbouring texts, without which occurs failure of loading long texts.
+                        width: 60, // Not sure what unit is using here.
+                        overflow: 'truncate', // Truncate long text with default ellipsis (…).
                     },
                     splitArea: {
                         show: true
@@ -240,10 +311,10 @@
                     type: 'category',
                     data: yLegend,
                     inverse: true,
-                    axisLabel:{
-                        interval:0,
-                        width: 60,
-                        overflow: 'truncate',
+                    axisLabel:{ // Text on Y axis.
+                        interval: 0, // Force to split neighbouring texts, without which occurs failure of loading long texts.
+                        width: 150, // Not sure what unit is using here.
+                        overflow: 'truncate', // Truncate long text with default ellipsis (…).
                     },
                     splitArea: {
                         show: true
@@ -252,31 +323,59 @@
                 visualMap: {
                     min: 50,
                     max: 80,
+                    dimension: 2, // Bind with item[2] in series.data
                     calculable: true,
                     orient: 'horizontal',
                     left: 'center',
                     bottom: '0%',
                     inRange: {
-                        color: ['#b0c0f0', '#1030a0']
+                        color: ['#b0c0f0', '#1030a0'] // Customized gradient from light-grayish-blue (min) to deep-cobalt-blue (max)
                     }
                 },
-                series: [{
-                    name: 'Similarity Score',
-                    type: 'heatmap',
-                    data: data,
-                    label: {
-                        show: true
-                    },
-                    emphasis: {
-                        itemStyle: {
-                            shadowBlur: 10,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                        }
-                    }
-                }]
+                series: dataSeriesArray,
+                // series: [{
+                //     name: '对齐选中',
+                //     type: 'heatmap',
+                //     data: dataHit,  // This series contains align results to be exported.
+                //     label: {
+                //         show: true
+                //     },
+                //     itemStyle: {    // Wrap cells with white border.
+                //         borderColor:'#ffffff',
+                //         borderWidth: 4,        
+                //     },
+                //     emphasis: {     // When cursor passes by.
+                //         itemStyle: {
+                //             shadowBlur: 10,
+                //             shadowColor: 'rgba(0, 0, 0, 0.5)',
+                //         }
+                //     }
+                // }, {
+                //     name: '对齐忽略',
+                //     type: 'heatmap',
+                //     data: dataMissed,   // This series contains align results to be abandoned.
+                //     label: {
+                //         show: true
+                //     },
+                //     selectedMode: 'multiple',
+                //     emphasis: {     // When cursor passes by.
+                //         itemStyle: {
+                //             shadowBlur: 10,
+                //             shadowColor: 'rgba(0, 0, 0, 0.5)',
+                //         }
+                //     }
+                // }]
             };
 
             option && myChart.setOption(option);
+            myChart.resize({
+                width: parseInt((xLegend.length * 80) + 160) + 'px',
+                height: parseInt((yLegend.length * 50) + 100) + 'px',
+            });
+
+            myChart.on('selectchanged', function (params) {
+                console.log(params);
+            });
         }
     }
   
