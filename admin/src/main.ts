d8f7dbed3b414af2a30f749b419917b087775fd7
Tien <zhao_t_y@sina.com>
2021-02-14 17:45:36 +0800

    Add production host to Vue axios

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -7,7 +7,8 @@ import axios from 'axios'
 Vue.config.productionTip = false
 
 Vue.prototype.$http = axios.create({
-  baseURL: 'http://localhost:3000'
+  // baseURL: 'http://localhost:3000', // Development Environment
+  baseURL: 'http://suoyan.tienzhao.com:3000' // Production Environment
 })
 
 new Vue({
